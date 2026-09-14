3d5c8253978e4669dddff981a2f5f9b28c68a4e6
santoshml-lab <santoshkrsbg36@gmail.com>
2026-09-15 02:15:29 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,1 +1,28 @@
+from groq import Groq
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+
+def ask_agent(question: str):
+    response = client.chat.completions.create(
+        model="openai/gpt-oss-20b",
+        messages=[
+            {
+                "role": "system",
+                "content": (
+                    "You are EduAgent AI, an intelligent education assistant. "
+                    "Answer questions clearly, accurately, and step-by-step."
+                ),
+            },
+            {
+                "role": "user",
+                "content": question,
+            },
+        ],
+    )
+
+    return response.choices[0].message.content
