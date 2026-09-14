d03114be2906c7158d09167a31dd3c86d7200547
santoshml-lab <santoshkrsbg36@gmail.com>
2026-09-15 02:17:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from agent import ask_agent
 
+app = FastAPI(title="EduAgent AI")
+
+
+class Question(BaseModel):
+    question: str
+
+
+@app.get("/")
+def home():
+    return {
+        "message": "EduAgent AI is running 🚀"
+    }
+
+
+@app.post("/ask")
+def ask_question(data: Question):
+    answer = ask_agent(data.question)
+
+    return {
+        "question": data.question,
+        "answer": answer
+    }
